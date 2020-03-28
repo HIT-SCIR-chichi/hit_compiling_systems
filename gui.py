014b5d39b5094462a450fd99a6eb84d488912170
chichi <2584363094@qq.com>
2020-03-29 00:24:31 +0800

    完成词法分析的正确情况处理+gui展示

--- gui.py
+++ gui.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
-from PyQt5.QtWidgets import QMenuBar, QApplication, QMenu, QMainWindow, QAction, QFileDialog, QStatusBar
-from PyQt5.QtGui import QIcon, QKeySequence
+from PyQt5.QtWidgets import QMenuBar, QApplication, QMenu, QMainWindow, QAction, QFileDialog, QDialog, QLabel, \
+    QTableWidget, QAbstractItemView, QTableWidgetItem, QHeaderView
+from PyQt5.QtWebEngineWidgets import QWebEngineView
+from PyQt5.QtGui import QIcon, QKeySequence, QFont
+from PyQt5.QtCore import QUrl, Qt
 from lexical import Lexical
-from editor import Editor
 import sys
 import os
 
@@ -12,13 +14,41 @@ height = 540  # 窗口高度
 bar_height = 26  # 菜单栏和状态栏高度
 
 
+# todo 修改QDialog右上角的问号
+# todo 添加nfa与dfa转换
+class Editor(QWebEngineView):
+    def __init__(self, par):
+        super().__init__(par)
+        self.text = ''  # 输入的文本
+        self.editor_flag = []
+
+        self.editor_index = os.getcwd() + '/help/index.html'
+        self.load(QUrl.fromLocalFile(self.editor_index))
+
+    def _callback(self, res):
+        self.text = res
+
+    def get_text(self, callback=_callback):
+        self.page().runJavaScript("monaco.editor.getModels()[0].getValue()", callback)
+
+    def set_text(self, data):
+        import base64
+        self.text = data
+        data = base64.b64encode(data.encode()).decode()  # 编解码代码文件
+        js_str = "monaco.editor.getModels()[0].setValue(Base64.decode('{}'))".format(data)
+        self.page().runJavaScript(js_str)
+
+    def change_language(self, lan):
+        self.page().runJavaScript("monaco.editor.setModelLanguage(monaco.editor.getModels()[0],'{}')".format(lan))
+
+
 class MainWindow(QMainWindow):
     def __init__(self, editor):
         super().__init__()
         self.editor = editor
         self.setFixedSize(width, height)
         self.setWindowTitle('编译器@zjr')
-        self.setWindowIcon(QIcon('./img/system.ico'))
+        self.setWindowIcon(QIcon('help/system.ico'))
         self.setCentralWidget(editor)
 
         self.file_path = None  # 打开的文件的目录
@@ -42,6 +72,8 @@ class MainWindow(QMainWindow):
         self.semantic_menu = QMenu('语义分析', self.menu_bar)
         self.about_menu = QMenu('关于', self.menu_bar)
         self.init_menu_bar()
+
+        self.lexical_window = None
 
     def init_menu_bar(self):
         self.menu_bar.setGeometry(0, 0, width, bar_height)
@@ -92,7 +124,12 @@ class MainWindow(QMainWindow):
         self.editor.get_text(self.__save_as_callback)
 
     def __lexical_run_callback(self, res):
-        print(res)
+        lexical = Lexical()
+        lexical.get_dfa('./input/dfa.json')
+        dfa_table = lexical.get_dfa_table()
+        lexical_res = lexical.lexical_run(res)  # 词法分析的token序列
+        self.lexical_window = LexicalWindow(lexical_res, dfa_table)
+        self.lexical_window.show()
 
     def lexical_run(self):  # 运行词法分析
         self.editor.get_text(self.__lexical_run_callback)
@@ -110,8 +147,60 @@ class MainWindow(QMainWindow):
         pass
 
 
+class LexicalWindow(QDialog):
+    def __init__(self, lexical_res, dfa_table):
+        super().__init__()
+        self.__res_table = QTableWidget(len(lexical_res) if len(lexical_res) > 0 else 1, 4, self)  # 词法分析表
+        self.__dfa_table = QTableWidget(dfa_table[0], len(dfa_table[1]), self)  # DFA转换表
+        self.__result_label = QLabel('词法分析结果', self)
+        self.__dfa_label = QLabel('DFA转换表', self)
+
+        self.__set_ui()
+        self.__set_res_table(lexical_res)
+        self.__set_dfa_table(dfa_table)
+
+    def __set_ui(self):
+        self.resize(width, height)
+        self.setWindowTitle('词法分析')
+        self.setWindowIcon(QIcon('./help/system.ico'))
+
+        self.__result_label.setGeometry(0, 0, width / 2, bar_height * 2)
+        self.__result_label.setFont(QFont('roman times', 15))
+        self.__result_label.setAlignment(Qt.AlignCenter)
+
+        self.__dfa_label.setGeometry(width / 2, 0, width / 2, bar_height * 2)
+        self.__dfa_label.setFont(QFont('roman times', 15))
+        self.__dfa_label.setAlignment(Qt.AlignCenter)
+
+    def __set_res_table(self, lexical_res):
+        self.__res_table.setGeometry(0, bar_height * 2, width / 2, height - bar_height * 2)
+        self.__res_table.setHorizontalHeaderLabels(['行号', '字符串', 'Token序列', '类型'])
+        self.__res_table.setEditTriggers(QAbstractItemView.NoEditTriggers)
+        self.__res_table.horizontalHeader().setSectionResizeMode(QHeaderView.ResizeToContents)
+        for idx in lexical_res:
+            self.__res_table.setItem(idx, 0, QTableWidgetItem('0'))  # 行号
+            self.__res_table.setItem(idx, 1, QTableWidgetItem(lexical_res[idx][1]))  # 字符串
+            self.__res_table.setItem(idx, 2, QTableWidgetItem(
+                '<' + lexical_res[idx][0] + ' ,' + lexical_res[idx][1] + '>'))
+            self.__res_table.setItem(idx, 3, QTableWidgetItem(lexical_res[idx][0]))  # token类型
+
+    def __set_dfa_table(self, dfa_table):
+        state_num, all_char, dfa = dfa_table
+        all_char = ['换行符' if char == '\n' else char for char in all_char]
+        self.__dfa_table.setGeometry(width / 2, bar_height * 2, width / 2, height - bar_height * 2)
+        self.__dfa_table.setHorizontalHeaderLabels(all_char)
+        self.__dfa_table.setVerticalHeaderLabels([str(idx) for idx in range(0, state_num)])  # 注意str不可少
+        self.__dfa_table.setEditTriggers(QAbstractItemView.NoEditTriggers)
+        self.__dfa_table.horizontalHeader().setSectionResizeMode(QHeaderView.ResizeToContents)
+        for idx in range(0, state_num):
+            for idy, char in enumerate(all_char):
+                if idx in dfa and char in dfa[idx]:
+                    self.__dfa_table.setItem(idx, idy, QTableWidgetItem(str(dfa[idx][char])))  # 注意item值转换为str
+
+
 if __name__ == "__main__":
     app = QApplication(sys.argv)
     window = MainWindow(Editor(None))
     window.show()
+
     sys.exit(app.exec_())
--- lexical.py
+++ lexical.py
@@ -10,7 +10,8 @@ class Lexical:
         self.start_state = 0  # 开始状态编号
         self.end_state = {}  # 结束状态编号及其对应的描述
         self.key_word = []  # 关键字集合
-        self.char = ''  # 状态转移表中的所有终结符
+        self.char = []  # 状态转移表中的所有引起转移的符号终结符集合，包括'other'
+        self.chars = ''  # 状态转移表中的所有引起转移的符号终结符串，不包括'other'
         self.dfa = {}  # dfa转换表
         self.nfa = {}  # nfa转换表
         self.rules = {}  # 词法规则
@@ -23,6 +24,7 @@ class Lexical:
             self.end_state = {int(idx): dic['end_state'][idx] for idx in dic['end_state'].keys()}
             self.key_word = dic['key_word']
             self.char = dic['char']
+            self.chars = dic['chars']
             self.dfa = {idx: dic[str(idx)] for idx in range(0, self.state_number) if str(idx) in dic}
 
     def get_nfa(self, nfa_path):  # 从文件中获取nfa
@@ -37,10 +39,15 @@ class Lexical:
 
     def lexical_run(self, text):  # 基于DFA的词法分析
         res = {}  # 结果集合
+        if text == '':
+            return res
         state, idx_count, next_char = self.start_state, 0, text[0]  # 记录状态编号,字符长度,当前字符
         end_state, end_str, last_str = -1, '', ''  # 分析过程中保存的上一个终结状态和对应的分析串，last_str保存字串
-        while idx_count < len(text):
+        while idx_count <= len(text):
             (end_state, end_str) = (state, last_str) if state in self.end_state else (end_state, end_str)
+            if idx_count == len(text) and end_state != -1:
+                res[len(res)] = (self.end_state[end_state], end_str)
+                break
             next_char = text[idx_count]
             last_str += next_char
             idx_count += 1
@@ -49,7 +56,7 @@ class Lexical:
                 last_str = ''
                 continue
             if state in self.dfa:
-                if next_char in self.char:
+                if next_char in self.chars:
                     for key in self.dfa[state]:
                         if next_char in key and key is not 'other':
                             state = self.dfa[state][key]  # 根据状态表更新状态
@@ -59,10 +66,20 @@ class Lexical:
                     state = self.dfa[state]['other']
                     flag = True
             if not flag:
-                res[len(res)] = (self.end_state[end_state], end_str)  # 保存结果
-                print(res[len(res) - 1])
+                if end_state == -1:
+                    idx_count += 1  # todo
+                    print('ERROR')
+                else:
+                    if self.end_state[end_state] == '标识符关键字':
+                        res[len(res)] = (end_str.upper(), '_') if end_str in self.key_word else ('标识符', end_str)
+                    else:
+                        res[len(res)] = (self.end_state[end_state], end_str)
                 state, idx_count = 0, idx_count - 1  # 更新自动机状态与指针
                 end_state, end_str, last_str = -1, '', ''  # 更新分析过程中保存的终结状态与对应字串,last_str表示累计的字串
+        return res
+
+    def get_dfa_table(self):
+        return self.state_number, self.char, self.dfa
 
 
 if __name__ == '__main__':
