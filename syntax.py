15715c46e46546e5aa06377e524dc0472166c8a0
1172510217 <2584363094@qq.com>
2020-04-11 22:16:59 +0800

    :sparkles:实现计算Select集功能

--- syntax.py
+++ syntax.py
@@ -10,9 +10,10 @@ class Syntax:
         self.empty_str = ''  # 文法中的空串表示
         self.terminals = []  # 终结符号集合
         self.non_terminals = []  # 非终结符集合
-        self.rules = {}  # 文法规则
+        self.rules = []  # 所有的文法规则
         self.first = {}  # 所有文法符号的First集合
         self.follow = {}  # 所有文法符号的Follow集合
+        self.select = []  # 所有产生式的Select集合
 
     def read_syntax(self, json_path: str):
         with open(json_path, 'r', encoding='utf-8') as f:
@@ -21,7 +22,9 @@ class Syntax:
             self.empty_str = js_dic['empty_string']
             self.terminals = js_dic['terminals']
             self.non_terminals = js_dic['non_terminals']
-            self.rules = {symbol: [item.split() for item in value] for (symbol, value) in js_dic['rules'].items()}
+            for key, values in js_dic['rules'].items():
+                for value in values:
+                    self.rules.append((key, value.split()))
 
     def get_first(self):
         """获得文法中所有符号的first集."""
@@ -31,26 +34,24 @@ class Syntax:
         flag = True  # 迭代更新标志，用于判断是否仍需要一轮迭代
         while flag:
             flag = False
-            for non_terminal in self.non_terminals:  # 对每一个非终结符求解First集合
-                productions = self.rules[non_terminal]  # 产生式集合
-                for production in productions:  # 遍历每一个产生式
-                    if production == [self.empty_str]:
-                        if self.empty_str not in self.first[non_terminal]:  # 存在未加入到First集中的空串，将其加入到First集合
-                            self.first[non_terminal].append(self.empty_str)
-                            flag = True
-                        continue
-                    for symbol in production:  # 遍历非空产生式右部的每一个符号
-                        for item in self.first[symbol]:  # 如果是空串，则说明需要处理下一个符号，同时不添加空串到First集中
-                            if item != self.empty_str and item not in self.first[non_terminal]:
-                                self.first[non_terminal].append(item)
-                                flag = True
-                        if self.empty_str in self.first[symbol]:  # 右部符号First集含有空串，分析下个符号或将空串加入
-                            continue
-                        else:  # 右部符号First集不含有空串，说明该产生式分析结束
-                            break
-                    if self.empty_str in self.first[symbol] and self.empty_str not in self.first[non_terminal]:
-                        self.first[non_terminal].append(self.empty_str)  # 若产生式最终可以推导出空串，则将空串加入到First集
+            for non_terminal, production in self.rules:  # 遍历每一个产生式
+                if production == [self.empty_str]:
+                    if self.empty_str not in self.first[non_terminal]:  # 存在未加入到First集中的空串，将其加入到First集合
+                        self.first[non_terminal].append(self.empty_str)
                         flag = True
+                    continue
+                for symbol in production:  # 遍历非空产生式右部的每一个符号
+                    for item in self.first[symbol]:  # 如果是空串，则说明需要处理下一个符号，同时不添加空串到First集中
+                        if item != self.empty_str and item not in self.first[non_terminal]:
+                            self.first[non_terminal].append(item)
+                            flag = True
+                    if self.empty_str in self.first[symbol]:  # 右部符号First集含有空串，分析下个符号或将空串加入
+                        continue
+                    else:  # 右部符号First集不含有空串，说明该产生式分析结束
+                        break
+                if self.empty_str in self.first[symbol] and self.empty_str not in self.first[non_terminal]:
+                    self.first[non_terminal].append(self.empty_str)  # 若产生式最终可以推导出空串，则将空串加入到First集
+                    flag = True
 
     def get_str_first(self, symbols: list) -> list:
         """获取文法符号串的First集.
@@ -83,21 +84,36 @@ class Syntax:
         flag = True  # 迭代更新标志，用于判断是否仍需要一轮迭代
         while flag:
             flag = False
-            for non_terminal in self.non_terminals:
-                for production in self.rules[non_terminal]:  # 遍历每一个产生式
-                    if production != [self.empty_str]:  # 空产生式不会更新Follow集
-                        for idx, symbol in enumerate(production):
-                            if symbol in self.non_terminals:  # 跳过终结符
-                                str_first = self.get_str_first(production[idx + 1:])
-                                for item in str_first:
-                                    if item != self.empty_str and item not in self.follow[symbol]:
-                                        self.follow[symbol].append(item)  # 加入symbol后所有文法符号的First集中未出现的元素
+            for non_terminal, production in self.rules:  # 遍历每一个产生式
+                if production != [self.empty_str]:  # 空产生式不会更新Follow集
+                    for idx, symbol in enumerate(production):
+                        if symbol in self.non_terminals:  # 跳过终结符
+                            str_first = self.get_str_first(production[idx + 1:])
+                            for item in str_first:
+                                if item != self.empty_str and item not in self.follow[symbol]:
+                                    self.follow[symbol].append(item)  # 加入symbol后所有文法符号的First集中未出现的元素
+                                    flag = True
+                            if not str_first or self.empty_str in str_first:  # 后续文法符号可以产生空串或者无后续符号
+                                for item in self.follow[non_terminal]:
+                                    if item not in self.follow[symbol]:
+                                        self.follow[symbol].append(item)
                                         flag = True
-                                if not str_first or self.empty_str in str_first:  # 后续文法符号可以产生空串或者无后续符号
-                                    for item in self.follow[non_terminal]:
-                                        if item not in self.follow[symbol]:
-                                            self.follow[symbol].append(item)
-                                            flag = True
+
+    def get_select(self):
+        """获得文法中所有产生式的Select集.
+
+        要求syntax对象已经执行过get_first()和get_follow()方法.
+        """
+        for idx, (non_terminal, production) in enumerate(self.rules):  # 遍历每一个产生式
+            if production == [self.empty_str]:  # 空产生式的select集为左部的Follow集
+                self.select.append(self.follow[non_terminal])
+            else:
+                str_first = self.get_str_first(production)  # 计算A->α中的α的First集
+                if self.empty_str not in str_first:
+                    self.select.append(str_first)
+                else:  # First集中存在空串，则select集为(First(α)-{空串}) ∪ Follow(A)，这里采用set的union方法操作
+                    self.select.append(
+                        set(item for item in str_first if item != self.empty_str).union(self.follow[non_terminal]))
 
 
 def main():
@@ -105,7 +121,7 @@ def main():
     syntax.read_syntax('help/syntax.json')
     syntax.get_first()
     syntax.get_follow()
-    print(syntax.follow)
+    syntax.get_select()
 
 
 if __name__ == '__main__':
