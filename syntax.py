36942e500dfb75e1757cdf3025d9516532019552
1172510217 <2584363094@qq.com>
2020-04-12 14:32:57 +0800

    :sparkles:完善语法分析（附带错误处理）；完善预测表（附带同步词法单元）

--- syntax.py
+++ syntax.py
@@ -8,13 +8,15 @@ class Syntax:
     def __init__(self):
         self.start_symbol = ''  # 文法开始符号
         self.empty_str = ''  # 文法中的空串表示
+        self.syn_token = ''  # 同步词法单元标识
         self.terminals = []  # 终结符号集合
         self.non_terminals = []  # 非终结符集合
         self.rules = []  # 所有的文法规则
         self.first = {}  # 所有文法符号的First集合
         self.follow = {}  # 所有文法符号的Follow集合
         self.select = []  # 所有产生式的Select集合
-        self.prediction_table = {}  # 预测分析表{non_terminal: {terminal: production}}
+        self.predict = {}  # 预测分析表{non_terminal: {terminal: production}}
+        self.tree = None  # 语法分析树
 
     def read_syntax(self, json_path: str):
         with open(json_path, 'r', encoding='utf-8') as f:
@@ -22,10 +24,12 @@ class Syntax:
             self.start_symbol = js_dic['start_symbol']
             self.empty_str = js_dic['empty_string']
             self.terminals = js_dic['terminals']
+            self.syn_token = js_dic['syn_token']
             self.non_terminals = js_dic['non_terminals']
             for non_terminal, values in js_dic['rules'].items():
                 for value in values:
                     self.rules.append((non_terminal, value.split()))
+            self.tree = SyntaxNode(self.start_symbol)
 
     def get_first(self):
         """获得文法中所有符号的first集."""
@@ -117,42 +121,65 @@ class Syntax:
                         set(item for item in str_first if item != self.empty_str).union(self.follow[non_terminal]))
 
     def get_prediction_table(self):
-        """构造LL(1)预测分析表."""
+        """构造LL(1)预测分析表.
+
+        若不是LL(1)文法，则返回False；否则计算相应的预测分析表.
+        """
         for idx, (non_terminal, rule) in enumerate(self.rules):
-            if non_terminal not in self.prediction_table:
-                self.prediction_table[non_terminal] = {terminal: idx for terminal in self.select[idx]}
+            if non_terminal not in self.predict:
+                self.predict[non_terminal] = {terminal: idx for terminal in self.select[idx]}
             else:
                 for terminal in self.select[idx]:
-                    if terminal in self.prediction_table[non_terminal]:
+                    if terminal in self.predict[non_terminal]:
                         return False
-                    self.prediction_table[non_terminal][terminal] = idx
+                    self.predict[non_terminal][terminal] = idx
+        for non_term, follow in self.follow.items():
+            for term in follow:
+                if term not in self.predict[non_term]:
+                    self.predict[non_term][term] = self.syn_token
 
     def syntax_run(self, text: list):
         """运行语法分析.
 
-        要求LL(1)预测分析表已经构建完成.
+        要求LL(1)预测分析表已经构建完成，若遇到错误，执行错误处理.
 
         Args:
             text: 待分析的输入符号列表，序号越大代表输入符号越靠后（右）
-        Returns:
-            若遇到错误，返回False；否则返回None
         """
-        text, stack = text + ['$'], [self.start_symbol, '$']  # 输入符号，分析栈
+        text, stack, node_stack = text + ['$'], [self.start_symbol, '$'], [self.tree, '$']  # 输入符号，分析栈，节点栈
         while stack:
             if stack[0] == text[0]:  # 栈顶元素与待分析串第一个元素相同，执行出栈操作
+                stack, text, node_stack = stack[1:], text[1:], node_stack[1:]
+            elif stack[0] in self.terminals:  # 栈顶终结符与当前输入符号不匹配
+                print('错误的终结符，并忽略该符号栈栈顶终结符', stack[0])
                 stack.pop(0)
-                text = text[1:]
-            elif stack[0] in self.terminals:
-                return False
-            elif text[0] not in self.prediction_table[stack[0]]:  # 对应表项不存在
-                return False
-            elif self.prediction_table[stack[0]][text[0]] == 'ERROR':  # 对应表项条目为报错信息
-                return False
+            elif text[0] not in self.predict[stack[0]]:  # 对应表项不存在
+                print('对应表项不存在', stack[0], text[0])
+                text.pop(0)
+            elif self.predict[stack[0]][text[0]] == self.syn_token:  # 对应表项条目为同步此法单元，则需要弹出符号栈栈顶非终结符
+                print('对应表项为同步此法单元', stack[0], text[0])
+                stack.pop(0)
             else:  # 栈顶非终结符与当前输入终结符找到对应表项
-                symbols = self.rules[self.prediction_table[stack[0]][text[0]]][1]  # 产生式右部符号
+                symbols = self.rules[self.predict[stack[0]][text[0]]][1]  # 产生式右部符号
                 print(stack[0] + '->' + ''.join(symbols))
+                child_nodes = [SyntaxNode(symbol) for symbol in symbols]
                 stack.pop(0)
+                node_stack.pop(0).add(child_nodes)
                 stack = stack if symbols == [self.empty_str] else symbols + stack
+                node_stack = node_stack if symbols == [self.empty_str] else child_nodes + node_stack
+
+
+class SyntaxNode:
+
+    def __init__(self, symbol, children=None):
+        self.symbol = symbol  # 当前节点的文法符号
+        self.children = [] if children is None else children  # 当前文法符号产生式的右部
+
+    def add(self, children):
+        self.children.extend(children)
+
+    def __str__(self):
+        return self.symbol + '->' + ''.join([child.symbol for child in self.children]) if self.children else ''
 
 
 def main():
@@ -162,7 +189,7 @@ def main():
     syntax.get_follow()
     syntax.get_select()
     syntax.get_prediction_table()
-    syntax.syntax_run(['id', '+', 'id', '*', 'id'])
+    syntax.syntax_run(['+', '*', '+', 'id'])
 
 
 if __name__ == '__main__':
